a31cdd7bbccd4e2c5cfe0cb42ccc241e8b162ac3
jangsejin <sejin-011@hanmail.net>
2019-04-15 13:21:51 +0900

    firt commit

---
+++ app/src/main/java/com/hour24/tb/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.hour24.tb
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hour24.teamblind.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
